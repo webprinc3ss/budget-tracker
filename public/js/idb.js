26a771986950b996f54bc920cfaca8a51e99cfef
Mystic Kitty <djabranton@gmail.com>
2021-02-11 10:24:11 -0800

    Starting service worker.

--- public/js/idb.js
+++ public/js/idb.js
@@ -7,7 +7,7 @@ const request = indexedDB.open('budget_tracker', 1);
 request.onupgradeneeded = function (event) {
     // save a reference to the database 
     const db = event.target.result;
-    // create an object store (table) called pending (not)`new_pizza`, set it to have an auto incrementing primary key of sorts 
+    // create an object store (table) called pending, set it to have an auto incrementing primary key of sorts 
     db.createObjectStore('pending', { autoIncrement: true });
 };
 
