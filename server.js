8d081cbc4554e162e8ce2e37882e1fbb1ca88a30
Mystic Kitty <djabranton@gmail.com>
2021-02-12 12:47:11 -0800

    Created mongo db connection on server.js.

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+// const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
+  useFindAndModify: false,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 const app = express();
 
